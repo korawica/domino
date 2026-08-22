dc1cfd9811a29524a20498c0f24d26d7971e58aa
Korawich Anuttra <korawich.anu@gmail.com>
2026-08-22 11:56:32 +0700

    feat: update label model

--- domino/models/label.py
+++ domino/models/label.py
@@ -1,1 +1,20 @@
-class Label: ...
+from typing import Literal
+
+from pydantic import BaseModel, Field
+
+PriorityType = Literal["p1", "p2", "p3"]
+
+
+class Label(BaseModel):
+    team: str = Field(
+        default="notset",
+        description="The team responsible for the task.",
+    )
+    system: str = Field(
+        default="notset",
+        description="The system responsible for the task.",
+    )
+    priority: PriorityType = Field(
+        default="p3",
+        description="The priority of the task.",
+    )
