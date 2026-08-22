df785e5d72ba6b9513fd45204411788eda9c0572
Korawich Anuttra <korawich.anu@gmail.com>
2026-08-22 17:09:32 +0700

    update code

--- tests/utils/test_dotdict.py
+++ tests/utils/test_dotdict.py
@@ -4,3 +4,5 @@ from domino.utils.dotdict import DotDict
 def test_dotdict():
     d = DotDict(a={"c": 1}, b=2)
     assert d["a.c"] == 1
+    assert d["a.e?"] is None
+    assert d.get("a.e?.f", "default") == "default"
