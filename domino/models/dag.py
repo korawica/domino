2f75877895141328dc2e6bde1b9672b4b92e79ff
Korawich Anuttra <korawich.anu@gmail.com>
2026-08-22 23:50:01 +0700

    update code

--- domino/models/dag.py
+++ domino/models/dag.py
@@ -1,4 +1,4 @@
-from typing import Literal
+from typing import Any, Literal
 
 from airflow import DAG
 from pydantic import BaseModel, Field
@@ -27,10 +27,43 @@ class Dag(BaseModel):
         description="A set of labels associated with the DAG.",
     )
 
+    schedule: str | None = Field(
+        default=None,
+        description="The schedule interval for the DAG, in cron format or a preset.",
+    )
+    start_date: str | None = Field()
+    end_date: str | None = Field()
+    catchup: bool | None = Field(
+        default=None,
+        description="Whether the DAG should catch up on missed runs.",
+    )
+
+    max_active_tasks: int | None = Field(
+        default=None,
+        description="The maximum number of active tasks for the DAG.",
+    )
+    max_active_runs: int | None = Field(
+        default=None,
+        description="The maximum number of active runs for the DAG.",
+    )
+    max_consecutive_failed_dag_runs: int | None = Field(
+        default=None,
+        description="The maximum number of consecutive failed DAG runs.",
+    )
+    dagrun_timeout_sec: int | None = Field(
+        default=None,
+        description="The timeout for a DAG run, in seconds.",
+    )
+
     # Task models
     tasks: list[Task] = Field(
         description="A list of tasks associated with the DAG.",
     )
+
+    def dag_kwargs(self) -> dict[str, Any]:
+        return self.model_dump(
+            exclude_unset=True,
+        )
 
     def build(
         self,
@@ -38,5 +71,6 @@ class Dag(BaseModel):
         """Build the Airflow DAG from the DAG model."""
         dag = DAG(
             dag_id=self.id,
+            **self.dag_kwargs(),
         )
         return dag
